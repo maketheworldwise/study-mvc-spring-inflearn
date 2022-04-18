51fc8433b3710638a8e12cb7b7873f67b1970209
Kevin <maketheworldwise@gmail.com>
2022-04-18 20:45:21 +0900

    :art: HandlerInterceptor

--- src/main/java/com/example/application/WebConfig.java
+++ src/main/java/com/example/application/WebConfig.java
@@ -2,6 +2,7 @@ package com.example.application;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -12,4 +13,13 @@ public class WebConfig implements WebMvcConfigurer {
 //        registry.addFormatter(new PersonFormatter());
 //    }
 
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        // Order 옵션을 주지 않으면 Add한 순서대로 적용
+        registry.addInterceptor(new GreetingInterceptor()).order(0);
+        registry.addInterceptor(new AnotherInterceptor())
+                .addPathPatterns("/hi/")
+                .order(-1);
+    }
 }
