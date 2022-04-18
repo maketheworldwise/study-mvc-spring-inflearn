4bdb07c984b9fd126c13a6670d09396432702f3d
Kevin <maketheworldwise@gmail.com>
2022-04-18 17:14:12 +0900

    :art: Formatter 어노테이션으로 빈 등록
    빈으로 직접 등록하는 방법이 아닌 어노테이션으로 빈을 등록하는 방법

--- src/test/java/com/example/application/SimpleControllerTest.java
+++ src/test/java/com/example/application/SimpleControllerTest.java
@@ -3,7 +3,9 @@ package com.example.application;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -12,7 +14,12 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest
+// @WebMvcTest는 웹과 관련된 빈을 등록해주기 때문에 PersonFormatter 빈을 등록해주지 않음
+// - 명시적으로 빈을 등록해서 해결
+// - 통합 테스트로 변경하여 모든 빈들을 등록
+@SpringBootTest
+@AutoConfigureMockMvc
+//@WebMvcTest
 public class SimpleControllerTest {
 
     @Autowired
